c9cb970bb5abda5f0584b2ee3bdb12c65800ba8e
nolangh <nolanghiggins@gmail.com>
2022-04-04 10:14:38 -0600

    -a

--- js/api.js
+++ js/api.js
@@ -1,11 +1,9 @@
-//TODO make start page
-const apiCards = "https://api.pokemontcg.io/v2/cards?page=1&pageSize=16";
+const apiCards = "https://api.pokemontcg.io/v2/cards?page=1&pageSize=32";
 const apiSets = "https://api.pokemontcg.io/v2/sets";
 const apiKey = "ef72570ff371408f9668e414353b7b2e";
-//This function grabs the cards
 
+//This function grabs the cards
 function getCards() {
-	let cards;
 	return fetch(apiCards, {
 		method: "GET",
 		headers: {
@@ -16,14 +14,13 @@ function getCards() {
 			return response.json();
 		})
 		.then((pokemon) => {
-			cards = pokemon;
+			const cards = pokemon;
 			console.log(cards);
 		});
 }
 
 //this function grabs the sets
-
-let cardSets = [];
+/*let cardSets = [];
 
 function getSets() {
 	return fetch(apiSets, {
@@ -44,13 +41,19 @@ function getSets() {
 		});
 }
 
-getSets();
 
 function selectSet() {
-	let testSelectHTML = "";
+	let testSelectHtml = "";
 	cardSets.forEach((x) => {
 		testSelectHTML += `<option value="${x.id}">${x.name}</option>`;
 	});
 
 	document.querySelector("testSelect").innerHTML = testSelectHtml;
+} */
+
+function render() {
+	//getSets();
+	getCards();
 }
+
+render();

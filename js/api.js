cd4d162df8c7e4ea3a87abbacd3beeef857c7def
nolangh <nolanghiggins@gmail.com>
2022-04-30 12:37:21 -0600

    trying to fix bug that hsowed up

--- js/api.js
+++ js/api.js
@@ -2,49 +2,16 @@
 const apiCards = "https://api.pokemontcg.io/v2/cards?page=1&pageSize=32";
 const apiSets = "https://api.pokemontcg.io/v2/sets";
 const apiKey = "ef72570ff371408f9668e414353b7b2e";
-const pos = document.getElementById("selectSet");
 const pokeCards = []; //this is the array that has each pokemon
 let cardSets = [];
-let allPokemon = [];
-let goodLord = [];
-let setName;
-
-/* ------------------ NOTE this will be moved to a new file ----------------- */
-
-const card = document.querySelector("card");
-const cardContainer = document.getElementById("card-cont");
-//const submit = document.querySelector("set-select-button");
-
-/* -----------------------------ANCHOR Event Listners ----------------------------- */
-pos.addEventListener("click", () => {
-	setName = pos.options[pos.selectedIndex].value;
-	return fetch(`https://api.pokemontcg.io/v2/cards?q=set.id:${setName}`, {
-		method: "GET",
-		headers: {
-			"X-api-key": apiKey,
-		},
-	})
-		.then((res) => {
-			return res.json();
-		})
-		.then((filteredSet) => {
-			allPokemon = filteredSet;
-			console.log(allPokemon);
-		})
-		.then(() => {
-			allPokemon.data.forEach((data) => {
-				createCard(data);
-			});
-		});
-});
 
 //ANCHOR creates pokemon object
 function Pokemon(image, name) {
-	this.pokemonImage = image;
-	this.pokemonName = name;
+	this.url = image;
+	this.name = name;
 }
 
-/* ------------------ ANCHOR this function grabs the cards from the api ------------------ */
+//ANCHOR this function grabs the cards
 function getCards() {
 	return fetch(apiCards, {
 		method: "GET",
@@ -52,20 +19,21 @@ function getCards() {
 			"X-Api-Key": apiKey,
 		},
 	})
-		.then((res) => {
-			return res.json();
+		.then((response) => {
+			return response.json();
 		})
 		.then((pokemon) => {
 			let cards = pokemon;
 			cards.data.forEach((data) => {
-				//pulls the images for the cards
-				let newPokemon = new Pokemon(data.images.large, data.name);
+				let newPokemon = new Pokemon(data.images.small, data.name);
 				pokeCards.push(newPokemon);
 			});
 		});
 }
 
-/* ------------------- ANCHOR this function grabs the sets ------------------ */
+console.log(pokeCards);
+
+//ANCHOR this function grabs the sets ignore anything below this line, this is for page 3
 function getSets() {
 	return fetch(apiSets, {
 		method: "GET",
@@ -84,34 +52,29 @@ function getSets() {
 		});
 }
 
-/* --------------------- ANCHOR populates select element -------------------- */
-
 function selectSet() {
 	let testSelectHtml = "";
 	cardSets.data.forEach((data) => {
 		testSelectHtml += `<option value="${data.id}">${data.name}</option>`;
 	});
 	document.getElementById("selectSet").innerHTML = testSelectHtml;
+	// $("#selectSet").text(testSelectHtml);
 }
 
-/* ----------------------- NOTE Card building function ---------------------- */
-
-function createCard(data) {
-	const pokeCardDiv = document.createElement("div");
-	pokeCardDiv.classList.add("card");
-
-	const pImage = document.createElement("img");
-	pImage.classList.add("card-img-top");
-	pImage.src = data.images.large;
-	pokeCardDiv.appendChild(pImage);
-	cardContainer.appendChild(pokeCardDiv);
+/* ----------------------- ANCHOR Randomizer function -----------------------
+function random(arr) {
+	// const randomIndex = Math.floor(Math.random() * arr.length);
+	// const item = arr[randomIndex];
+	// return item;
 }
+
+const result = random(pokeCards); */
 
 /* ------------------NOTE This is just a render function ----------------- */
-
 function render() {
 	getSets();
 	getCards();
+	//random();
 }
 
 render();
--- js/function.js
+++ js/function.js
@@ -1,53 +1,144 @@
 let screenMode;
+let game = {
+	status: "setup",
+	players: 2,
+	player: 1,
+	dimensions: [],
+	fMatch: [""],
+	sMatch: [""],
+	scores: [],
+	cards: [
+		{
+			name: "pikachu",
+			index: 0,
+			url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+			active: true,
+			visible: false,
+		},
+		{
+			name: "pikachu",
+			index: 0,
+			url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+			active: true,
+			visible: false,
+		},
+	],
+};
 
 function startMenu() {
+	$("main").empty();
 	$("main").append(
 		`<button type="button" name="button" onclick="setBoard(4,3)">Start Game</button>`
 	);
 	screenMode = "start menu";
 }
+function setBoard(x, y, players) {
+	score = 0;
+	game.dimensions = [x, y];
+	game.players = players;
+	pokemonList = pokeCards;
+	game.status = "play";
 
-$("main").attr("id", "game-board");
-screenMode = "play";
-let cardSize = 40;
-let myCards = [];
-for (let i = 0; i < (x * y) / 2; i++) {
-	let currPokemon = Math.floor(Math.random() * pokemonTemp.length);
-	for (let j = 0; j < 2; j++) {
-		myCards.splice(Math.floor(Math.random() * myCards.length), 0, currPokemon);
+	for (let i = 0; i < players; i++) {
+		game.scores[i] = 0;
+	}
+
+	let myCards = [[]];
+	for (let i = 0; i < (x * y) / 2; i++) {
+		let currPokemon = Math.floor(Math.random() * pokemonList.length);
+		for (let j = 0; j < 2; j++) {
+			myCards.splice(Math.floor(Math.random() * myCards.length), 0, {
+				name: pokemonList[currPokemon].name,
+				index: currPokemon,
+				url: pokemonList[currPokemon].url,
+				active: true,
+				visible: true,
+			});
+		}
+	}
+	game.cards = myCards;
+	printCards(game.cards);
+
+	setTimeout(flipAll, 2500);
+	setTimeout(setToPlay, 2500);
+}
+
+function flipCard(index) {
+	if (game.cards[index].visible) {
+		game.cards[index].visible = false;
+	} else {
+		game.cards[index].visible = true;
+	}
+	update();
+}
+function flipMatches() {
+	game.status = "pause";
+	// purr(`flipping cards ${game.fMatch[1]} and ${game.sMatch[1]}`)
+	flipCard(game.fMatch[1]);
+	flipCard(game.sMatch[1]);
+	game.fMatch = [""];
+	game.sMatch = [""];
+	setToPlay();
+}
+function removeMatches() {
+	game.status = "pause";
+	// purr(`removing cards ${game.fMatch[1]} and ${game.sMatch[1]}`)
+	removeCard(game.fMatch[1]);
+	removeCard(game.sMatch[1]);
+	game.fMatch = [""];
+	game.sMatch = [""];
+	setToPlay();
+}
+function seeIfMatch(index) {
+	if (game.fMatch[0] == "") {
+		game.fMatch[0] = game.cards[index].name;
+		game.fMatch[1] = index;
+	} else if (game.sMatch == "") {
+		game.sMatch[0] = game.cards[index].name;
+		game.sMatch[1] = index;
+		if (game.fMatch[0] == game.sMatch[0]) {
+			game.status = "pause";
+			setTimeout(removeMatches, 500);
+			nextTurn(1);
+		} else {
+			game.status = "pause";
+			setTimeout(flipMatches, 500);
+			nextTurn(0);
+		}
+	} else {
+		purr(`match error`);
+	}
+}
+function haveMatch() {
+	if (game.fMatch[0] === game.sMatch[0]) {
+		return true;
+	} else {
+		return false;
 	}
 }
 
-//clears board of any leftover cards
-$("#game-board").empty();
-//Adds cards to board
-for (let i = 0; i < x * y; i++) {
-	$("#game-board").append(
-		`<div class="card" id="crd${i}">
-        <div class="inner-card hidden" id="inr${i}">
-          <h2 id="tit${i}">${pokemonTemp[myCards[i]].name}</h2>
-          <img src="${pokemonTemp[myCards[i]].url}" alt="${
-			pokemonTemp[myCards[i]].name
-		}" id="img${i}">
-        </div>
-      </div>`
-	);
+function nextTurn(score) {
+	game.scores[game.player - 1] += score;
+	if (game.player != game.players) {
+		game.player++;
+	} else {
+		game.player = 1;
+	}
 }
-//Makes card sizes all exact dimensions
-$(".card").css("width", `${2.5 * cardSize}px`);
-$(".card").css("height", `${3.5 * cardSize}px`);
-$(".card").css("border-radius", `${cardSize * 0.2}px`);
-//Makes height of board based on how many cards wide and high
-// Card ratio * Card Size + Card Margin multiplied by how many cards plus BOARD padding
-$("main").css("width", `${(2.5 * cardSize + 20) * x + 50}px`);
-$("main").css("height", `${(3.5 * cardSize + 20) * y + 50}px`);
-
-$(".card").click(function (e) {
-	let myID =
-		$(e.target).attr("id").charAt(3) + $(e.target).attr("id").charAt(4);
-	flipCard(myID);
-});
-
-function flipCard(index) {
-	$(`#inr${index}`).toggleClass("hidden");
+function removeCard(index) {
+	game.cards[index].active = false;
+	update();
+}
+function flipAll() {
+	for (let i = 0; i < game.dimensions[0] * game.dimensions[1]; i++) {
+		flipCard(i);
+	}
+	game.fMatch = [""];
+	game.sMatch = [""];
+}
+function setToPlay() {
+	game.status = "play";
+}
+function update() {
+	printCards(game.cards);
 }
